4ac16cded39c917c5299c61f78497b1273b26ccd
bartek9105 <bartek9105@gmail.com>
2021-02-03 18:28:50 +0100

    Fetch weight stats data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,11 +6,30 @@
 
 <script>
 import Hero from '@/components/Hero'
+import axios from 'axios'
 
 export default {
 	name: 'Home',
 	components: {
 		Hero
+	},
+	data() {
+		return {
+			stats: []
+		}
+	},
+	methods: {
+		async fetchWeightStats() {
+			try {
+				const statsData = await axios.get('http://localhost:5000/api/v1/stats')
+				this.stats = statsData.data
+			} catch (error) {
+				console.log(error)
+			}
+		}
+	},
+	created() {
+		this.fetchWeightStats()
 	}
 }
 </script>
