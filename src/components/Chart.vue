b169688aa94d2b624fc0be608dce584d3a01f720
bartek9105 <bartek9105@gmail.com>
2021-02-07 12:20:18 +0100

    Add chart component for weight tracking

---
+++ src/components/Chart.vue
@@ -0,0 +1,27 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+	name: 'Chart',
+	extends: Line,
+	props: {
+		chartStats: {
+			type: Object,
+			default: () => {}
+		}
+	},
+	data() {
+		return {
+			options: {
+				responsive: true,
+				maintainAspectRatio: false
+			}
+		}
+	},
+	mounted() {
+		this.renderChart(this.chartStats, this.options)
+	}
+}
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,56 @@
 <template>
 	<div>
 		<Hero />
+		<Chart v-if="!isLoading" :chart-stats="chartdata" />
 	</div>
 </template>
 
 <script>
 import Hero from '@/components/Hero'
+import Chart from '@/components/Chart'
 import axios from 'axios'
 
 export default {
 	name: 'Home',
 	components: {
-		Hero
+		Hero,
+		Chart
 	},
 	data() {
 		return {
-			stats: []
+			isLoading: true,
+			stats: [],
+			chartdata: {
+				labels: [],
+				datasets: [
+					{
+						label: 'Weight',
+						backgroundColor: '#f87979',
+						data: []
+					}
+				]
+			}
 		}
 	},
 	methods: {
 		async fetchWeightStats() {
 			try {
 				const statsData = await axios.get('http://localhost:5000/api/v1/stats')
-				this.stats = statsData.data
+				this.stats = statsData.data.stats
+				this.setChartData()
+				this.isLoading = false
 			} catch (error) {
 				console.log(error)
 			}
+		},
+		setChartData() {
+			this.stats.map((statsEl) => {
+				this.chartdata.labels.push(new Date(statsEl.createdAt).toDateString())
+				this.chartdata.datasets[0].data.push(statsEl.weight)
+			})
 		}
 	},
-	created() {
+	mounted() {
 		this.fetchWeightStats()
 	}
 }
